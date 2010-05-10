e8f08ad08dea3ba62bf8eeef2f6cfc52872d599e
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-05-10 17:41:36 +0000

    Don't track root file, set batch style

--- test/example_plot_macro.py
+++ test/example_plot_macro.py
@@ -12,6 +12,7 @@ import TauAnalysis.TauIdEfficiency.ntauples.plotting as plot
 
 
 if __name__ == "__main__":
+    ROOT.gROOT.SetBatch(True)
     file = ROOT.TFile("example_ntuple.root", "READ")
     
     # Get the events tree (this can also be a TChain)
