43dc4c7b0fa6dbc1b0ce9fc7e7421dfe240ba2f2
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-05-22 16:45:49 +0000

    added config snippet for QCD di-jet event selection

---
+++ python/filterQCDdiJet_cfi.py
@@ -0,0 +1,101 @@
+import FWCore.ParameterSet.Config as cms
+
+from TauAnalysis.TauIdEfficiency.filterDataQuality_cfi import *
+
+#--------------------------------------------------------------------------------
+# select QCD di-jet (/multi-jet) events
+# from which to determine tau fake-rates
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+#
+# define HLT trigger path
+#
+hltJet15U = cms.EDFilter("EventSelPluginFilter",
+    selector = cms.PSet(
+        pluginName = cms.string('hltJet15U'),             
+        pluginType = cms.string('TriggerResultEventSelector'),
+        src = cms.InputTag('TriggerResults::HLT'),
+        triggerPaths = cms.vstring('HLT_Jet15U')
+    )
+)
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+#
+# define loose CaloTau candidate/CaloJet selection
+#
+selectedCaloTaus = cms.EDFilter("CaloTauSelector",
+    src = cms.InputTag('caloRecoTauProducer'),
+    discriminators = cms.VPSet(),
+    cut = cms.string("abs(caloTauTagInfoRef().calojetRef().eta) < 2.5 & caloTauTagInfoRef().calojetRef().pt > 10."),
+    filter = cms.bool(True)
+)
+
+caloTauPairs = cms.EDProducer("DiCandidatePairProducer",
+    useLeadingTausOnly = cms.bool(False),
+    srcLeg1 = cms.InputTag('selectedCaloTaus'),
+    srcLeg2 = cms.InputTag('selectedCaloTaus'),
+    dRmin12 = cms.double(0.),
+    srcMET = cms.InputTag(''),
+    recoMode = cms.string(""),
+    scaleFuncImprovedCollinearApprox = cms.string('1'),                                  
+    verbosity = cms.untracked.int32(0)                                       
+)
+
+selectedCaloTauPairs = cms.EDFilter("DiCandidatePairSelector",
+    src = cms.InputTag('caloTauPairs'),
+    cut = cms.string("dR12 > 0.3"),
+    filter = cms.bool(True)                                     
+)
+
+caloTauSkimPath = cms.Path(
+    hltJet15U
+   + selectedCaloTaus + caloTauPairs + selectedCaloTauPairs
+   ##+ dataQualityFilters
+)
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+#
+# define loose PFTau candidate/PFJet selection
+#
+selectedPFTaus = cms.EDFilter("PFTauSelector",
+    src = cms.InputTag('shrinkingConePFTauProducer'),
+    discriminators = cms.VPSet(),                          
+    cut = cms.string("abs(pfTauTagInfoRef().pfjetRef().eta) < 2.5 & pfTauTagInfoRef().pfjetRef().pt > 10."),
+    filter = cms.bool(True)
+)
+
+pfTauPairs = cms.EDProducer("DiCandidatePairProducer",
+    useLeadingTausOnly = cms.bool(False),
+    srcLeg1 = cms.InputTag('selectedPFTaus'),
+    srcLeg2 = cms.InputTag('selectedPFTaus'),
+    dRmin12 = cms.double(0.),
+    srcMET = cms.InputTag(''),
+    recoMode = cms.string(""),
+    scaleFuncImprovedCollinearApprox = cms.string('1'),                                  
+    verbosity = cms.untracked.int32(0)                                       
+)
+
+selectedPFTauPairs = cms.EDFilter("DiCandidatePairSelector",
+    src = cms.InputTag('pfTauPairs'),
+    cut = cms.string("dR12 > 0.3"),
+    filter = cms.bool(True)                                     
+)
+
+pfTauSkimPath = cms.Path(    
+    hltJet15U
+   + selectedPFTaus + pfTauPairs + selectedPFTauPairs
+   ##+ dataQualityFilters
+)
+#--------------------------------------------------------------------------------
+
+qcdDiJetEventSelection = cms.untracked.PSet(
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring(
+            'caloTauSkimPath',
+            'pfTauSkimPath'
+        )
+    )
+)
