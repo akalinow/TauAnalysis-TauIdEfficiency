1039f52d88f6f3dfbcbfe4358ea01cdd3d1c81bb
Joshua James Swanson <sha1-8881b0a90e2d698b47914ccde7f42725e686d8ab@cern.ch>
2010-06-24 14:39:13 +0000

    Added deltaphi and deltaeta

--- test/commissioning/make_hps_resolution_plot.py
+++ test/commissioning/make_hps_resolution_plot.py
@@ -6,9 +6,12 @@ import ROOT
 from TauAnalysis.TauIdEfficiency.ntauples.PlotManager import PlotManager
 import TauAnalysis.TauIdEfficiency.ntauples.styles as style
 
+
+
 # Defintion of input files.
 import samples as samples
 import os
+import sys
 
 
 if __name__ == "__main__":
@@ -35,20 +38,48 @@ if __name__ == "__main__":
     # Get the shrinking ntuple
     hps_ntuple = ntuple_manager.get_ntuple(
         "patPFTausDijetTagAndProbeHPS")
+        
+    theSelection = hps_ntuple.expr('abs($jetEta) < 2.5') & hps_ntuple.expr('$byLeadTrackFinding > 0.5') & hps_ntuple.expr('$genMatch > 0.5') & hps_ntuple.expr('$byIsolationMedium > 0.5')
+
 
     canvas = ROOT.TCanvas("example", "example", 500, 500)
     
     pt_resol = plotter.distribution(
         expression=hps_ntuple.expr('($pt-$genPt)/$genPt'),
-        selection=hps_ntuple.expr('abs($jetEta) < 2.5') & hps_ntuple.expr('$byLeadTrackFinding > 0.5') & hps_ntuple.expr('$genMatch > 0.5'),
+        selection=theSelection,
         extra_labels = [style.ETA_CUT_LABEL_UPPER_LEFT],
-        binning=(50, -1, 1),
+        binning=(200, -0.5, 0.5),
         x_axis_title = "#tau p_{T} Resolution",
         logy=False
     )
     
     canvas.SaveAs("plots/pt_resol.png")
     canvas.SaveAs("plots/pt_resol.pdf")
+    
+    eta_resol = plotter.distribution(
+        expression=hps_ntuple.expr('$eta-$genEta'),
+        selection=theSelection,
+        extra_labels = [style.ETA_CUT_LABEL_UPPER_LEFT],
+        binning=(100, -0.01, 0.01),
+        x_axis_title = "#tau #eta Resolution",
+        logy=False
+    )
+    
+    canvas.SaveAs("plots/eta_resol.png")
+    canvas.SaveAs("plots/eta_resol.pdf")
+    
+    phi_resol = plotter.distribution(
+        expression=hps_ntuple.expr('$phi-$genPhi'),
+        selection=theSelection,
+        extra_labels = [style.ETA_CUT_LABEL_UPPER_LEFT],
+        binning=(100, -0.01, 0.01),
+        x_axis_title = "#tau #phi Resolution",
+        logy=False
+    )
+    
+    canvas.SaveAs("plots/phi_resol.png")
+    canvas.SaveAs("plots/phi_resol.pdf")
+
 
 
  
