6d714efe4c2c56c11ff66d4285e349f735153769
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-06-28 18:52:18 +0000

    Adding easy script to produce the tanc eff. plots

---
+++ test/commissioning/run_TaNC_eff_plots.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# Run all of the relevant TaNC eff plots and distributions
+
+python shrinkingConePlots.py -p jetPt -p -jetEta -p jetPhi -p TaNCQuarter -p TaNCHalf -p TaNCOne -v Pt -v Eta -v Phi
+
