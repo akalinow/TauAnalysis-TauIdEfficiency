26b4e816e2cc9cf329ae1d69b80b2b2a0a2e50c1
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-06-30 00:30:47 +0000

    Better version

--- test/commissioning/run_TaNC_eff_plots.sh
+++ test/commissioning/run_TaNC_eff_plots.sh
@@ -2,5 +2,39 @@
 
 # Run all of the relevant TaNC eff plots and distributions
 
-python shrinkingConePlots.py -p jetPt -p -jetEta -p jetPhi -p TaNCQuarter -p TaNCHalf -p TaNCOne -v Pt -v Eta -v Phi
+##### PLOTS FOR THE PAS #####
+python tanc_fakerates_for_pas.py &
+
+# Wait for completion
+wait
+
+############################
+##### PLOTS FOR THE AN #####
+############################
+
+# Control plots
+# (assume someone else will make these)
+#python shrinkingConePlots.py -p jetPt 
+#python shrinkingConePlots.py -p jetEta 
+#python shrinkingConePlots.py -p jetPhi 
+
+# Fake rate plots
+python shrinkingConePlots.py -p TaNCQuarter -v Pt -v Eta -v Phi &
+python shrinkingConePlots.py -p TaNCHalf -v Pt -v Eta -v Phi &
+python shrinkingConePlots.py -p TaNCOne -v Pt -v Eta -v Phi &
+wait
+
+##### Resolution plots #####
+python make_tanc_resolution_plot.py &
+
+##### ZTT efficiency plots #####
+# NB this makes plots for ZTT for all algos
+# I think Abdollah is running these
+#python plot_Ztautau_v3.py &
+
+
+# finish up
+wait
+
+
 
