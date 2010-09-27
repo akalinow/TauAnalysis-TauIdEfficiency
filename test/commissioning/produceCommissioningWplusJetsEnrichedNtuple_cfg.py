d4f3c609804ef1e1312af65cba194fddc65acb71
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-09-27 14:06:11 +0000

    first check-in of top-level config file for producing Ntuples for fake-rate measurement in W + jets events

---
+++ test/commissioning/produceCommissioningWplusJetsEnrichedNtuple_cfg.py
@@ -0,0 +1,282 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("prodCommissioningWplusJetsEnrichedNtuple")
+
+# import of standard configurations for RECOnstruction
+# of electrons, muons and tau-jets with non-standard isolation cones
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+#process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
+process.load('Configuration/StandardSequences/GeometryIdeal_cff')
+process.load('Configuration/StandardSequences/MagneticField_cff')
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+
+#--------------------------------------------------------------------------------
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        #'/store/relval/CMSSW_3_6_1/RelValZTT/GEN-SIM-RECO/START36_V7-v1/0021/F405BC9A-525D-DF11-AB96-002618943811.root',
+        #'/store/relval/CMSSW_3_6_1/RelValZTT/GEN-SIM-RECO/START36_V7-v1/0020/EE3E8F74-365D-DF11-AE3D-002618FDA211.root'
+        ##'rfio:/castor/cern.ch/user/v/veelken/CMSSW_3_6_x/skims/tauCommissioning/data/muTauSkim_1_1.root'
+        ##'rfio:/castor/cern.ch/user/v/veelken/CMSSW_3_6_x/skims/tauCommissioning/mcMinBias/muTauSkim_1_1.root'
+        'rfio:/castor/cern.ch/user/v/veelken/CMSSW_3_6_x/skims/tauCommissioning/mcQCDpt15/muTauSkim_1_1.root'
+    ),
+    skipEvents = cms.untracked.uint32(0)            
+)
+
+# print event content 
+process.printEventContent = cms.EDAnalyzer("EventContentAnalyzer")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+
+isMC = True # use for MC (except for samples from Spring'10 reprocessing)
+isSpring10 = True # use for Spring'10 reprocessed MC
+##isSpring10 = False # use for non-Spring'10 reprocessed MC
+##isMC = False # use for Data
+applyTrackDowngrade = False # default
+#applyTrackDowngrade = True # to be used for studies of systematic uncertainties only
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+# define GlobalTag to be used for event reconstruction
+# (only relevant for HPS tau reconstruction algorithm)
+if isMC:
+    process.GlobalTag.globaltag = cms.string('MC_36Y_V10::All')
+else:
+    process.GlobalTag.globaltag = cms.string('GR_R_36X_V12::All')
+#--------------------------------------------------------------------------------    
+
+#--------------------------------------------------------------------------------
+# define skimming criteria
+# (in order to be able to produce Tau Ntuple directly from unskimmed Monte Carlo/datasets;
+#  HLT muon trigger passed && global muon of Pt > 3 GeV within |eta| < 2.5
+#                          && either CaloJet or PFJet of Pt > 10 GeV within |eta| < 2.5)
+process.load('TauAnalysis.TauIdEfficiency.filterWplusJetsEnriched_cfi')
+if isMC:
+    process.dataQualityFilters.remove(process.hltPhysicsDeclared)
+    process.dataQualityFilters.remove(process.dcsstatus)
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+#
+# produce collections of objects needed as input for PAT-tuple production
+# (e.g. rerun reco::Tau identification algorithms with latest tags)
+#
+from TauAnalysis.TauIdEfficiency.tools.configurePrePatProduction import configurePrePatProduction
+
+configurePrePatProduction(process, applyTrackDowngrade = applyTrackDowngrade, addGenInfo = isMC)
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+#
+# produce PAT objects
+#
+from TauAnalysis.TauIdEfficiency.tools.configurePatTupleProduction import configurePatTupleProduction
+from TauAnalysis.TauIdEfficiency.tools.sequenceBuilder import buildWplusJetsEnrichedTauSequence
+
+# define muon selection criteria
+# (used for "cleaning" of CaloTau/PFTau collection)
+#
+# NOTE: only muons passing selection will be written to Tau Ntuple
+#
+##process.load("PhysicsTools.PatAlgos.selectionLayer1.muonSelector_cfi")
+##process.selectedPatMuons.cut = cms.string("isGlobalMuon & pt > 3 & abs(eta) < 2.5")
+
+process.load("PhysicsTools.PFCandProducer.Isolation.pfMuonIsolation_cff")
+from PhysicsTools.PFCandProducer.Isolation.tools_cfi import *
+process.pfmuIsoDepositPFCandidates = isoDepositReplace("muons", "particleFlow")
+process.prePatProductionSequence._seq = process.prePatProductionSequence._seq * process.pfmuIsoDepositPFCandidates
+
+process.load("PhysicsTools.PatAlgos.producersLayer1.muonProducer_cfi")
+process.patMuons.userIsolation.user = cms.VPSet(
+    cms.PSet( 
+        src = cms.InputTag("pfmuIsoDepositPFCandidates"),
+        deltaR = cms.double(0.4)
+    )
+)
+
+# "clean" CaloTau/PFTau collections
+# (i.e. remove CaloTaus/PFTaus overlapping with muons)
+process.load("PhysicsTools.PatAlgos.cleaningLayer1.tauCleaner_cfi")
+
+# remove jets outside kinematic range Pt > 10 GeV && |eta| < 2.5 from Tau Ntuple
+# (in order to speed-up plotting macros)
+patCaloTauCleanerPrototype = process.cleanPatTaus.clone(
+    preselection = cms.string(''),
+    checkOverlaps = cms.PSet(
+        muons = cms.PSet(
+           src                 = cms.InputTag("selectedPatMuons"),
+           algorithm           = cms.string("byDeltaR"),
+           preselection        = cms.string("isGlobalMuon"),
+           deltaR              = cms.double(0.7),
+           checkRecoComponents = cms.bool(False),
+           pairCut             = cms.string(""),
+           requireNoOverlaps   = cms.bool(True)
+        )
+    ),        
+    finalCut = cms.string(
+        'caloTauTagInfoRef().jetRef().pt() > 5 & abs(caloTauTagInfoRef().jetRef().eta()) < 2.5'
+    )
+)
+
+patPFTauCleanerPrototype = patCaloTauCleanerPrototype.clone(
+    finalCut = cms.string(
+        'pfTauTagInfoRef().pfjetRef().pt() > 5 & abs(pfTauTagInfoRef().pfjetRef().eta()) < 2.5'
+    )
+)
+
+retVal = configurePatTupleProduction(
+    process, patSequenceBuilder = buildWplusJetsEnrichedTauSequence,
+    patPFTauCleanerPrototype = patPFTauCleanerPrototype,
+    patCaloTauCleanerPrototype = patCaloTauCleanerPrototype,
+    addGenInfo = isMC
+)
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+#
+# produce Ntuple
+#
+process.load("TauAnalysis.TauIdEfficiency.ntupleConfigTrigger_cfi")
+process.load("TauAnalysis.TauIdEfficiency.ntupleConfigVertex_cfi")
+process.load("TauAnalysis.TauIdEfficiency.ntupleConfigMuon_cfi")
+process.load("TauAnalysis.TauIdEfficiency.ntupleConfigCaloTau_cfi")
+process.load("TauAnalysis.TauIdEfficiency.ntupleConfigPFTauFixedCone_cfi")
+process.load("TauAnalysis.TauIdEfficiency.ntupleConfigPFTauShrinkingCone_cfi")
+process.load("TauAnalysis.TauIdEfficiency.ntupleConfigPFTauHPS_cfi")
+process.load("TauAnalysis.TauIdEfficiency.ntupleConfigPFTauShrinkingConeEllipticPhotonIso_cfi")
+process.load("TauAnalysis.TauIdEfficiency.ntupleConfigGenJets_cfi")
+process.load("TauAnalysis.TauIdEfficiency.ntupleConfigGenPhaseSpaceEventInfo_cfi")
+
+process.ntupleProducer = cms.EDAnalyzer("ObjValEDNtupleProducer",
+                                        
+    ntupleName = cms.string("tauIdEffNtuple"),
+                                        
+    sources = cms.PSet(
+        # Grouping of sources is for convenience of specifying pluginTypes, etc
+
+        # variables indicating decision of HLT trigger paths
+        trigger = process.trigger_template.clone(
+            columns = cms.PSet(
+                hltL1Jet6U          = cms.string("HLT_L1Jet6U"),
+                hltJet15U           = cms.string("HLT_Jet15U"),
+                hltJet30U           = cms.string("HLT_Jet30U"),
+                hltJet50U           = cms.string("HLT_Jet50U"),
+                hltMinBiasBSC       = cms.string("HLT_MinBiasBSC"),
+                hltMinBiasBSCnoBPTX = cms.string("HLT_MinBiasBSC_NoBPTX"),
+                hltMu3              = cms.string("HLT_Mu3"),
+                hltMu5              = cms.string("HLT_Mu5"),                                    
+                hltMu9              = cms.string("HLT_Mu9")
+            )
+        ),                                              
+
+        # variables specifying x,y,z coordinates of primary event vertex
+        vertex = process.vertex_template,
+
+        # variables specific to Muons
+        muons_rec = process.muons_recInfo,              
+
+        # variables specific to CaloTaus                                            
+        caloTaus_rec = process.caloTaus_recInfo.clone(
+            src = cms.InputTag(retVal["caloTauCollection"])                       
+        ),
+
+        # variables specific to fixed cone PFTaus                                            
+        pfTausFixedCone_rec = process.pfTausFixedCone_recInfo.clone(
+            src = cms.InputTag(retVal["pfTauCollectionFixedCone"])                       
+        ),
+
+        # variables specific to shrinking cone PFTaus                                            
+        pfTausShrinkingCone_rec = process.pfTausShrinkingCone_recInfo.clone(
+            src = cms.InputTag(retVal["pfTauCollectionShrinkingCone"])                       
+        ),
+
+        # variables specific to PFTaus reconstructed by hadron + strips (HPS) algorithm                                           
+        pfTausHPS_rec = process.pfTausHPS_recInfo.clone(
+            src = cms.InputTag(retVal["pfTauCollectionHPS"])                       
+        ),
+
+        # variables specific to shrinking cone PFTaus
+        # reconstructed using ellipse for photon isolation
+        pfTausShrinkingConeEllPhotonIso_rec = process.pfTausShrinkingConeEllipticPhotonIso_recInfo.clone(
+            src = cms.InputTag(retVal["pfTauCollectionShrinkingConeEllipticPhotonIso"])                       
+        )
+    )
+)
+
+if isMC:
+    setattr(process.ntupleProducer.sources, "muons_gen", process.muons_genInfo)
+    process.caloTaus_genInfo.src = cms.InputTag(retVal["caloTauCollection"])
+    setattr(process.ntupleProducer.sources, "caloTaus_gen", process.caloTaus_genInfo)
+    process.pfTausFixedCone_genInfo.src = cms.InputTag(retVal["pfTauCollectionFixedCone"])
+    setattr(process.ntupleProducer.sources, "pfTausFixedCone_gen", process.pfTausFixedCone_genInfo)
+    process.pfTausShrinkingCone_genInfo.src = cms.InputTag(retVal["pfTauCollectionShrinkingCone"])
+    setattr(process.ntupleProducer.sources, "pfTausShrinkingCone_gen", process.pfTausShrinkingCone_genInfo)
+    process.pfTausHPS_genInfo.src = cms.InputTag(retVal["pfTauCollectionHPS"])
+    setattr(process.ntupleProducer.sources, "pfTausHPS_gen", process.pfTausHPS_genInfo)
+    process.pfTausShrinkingConeEllipticPhotonIso_genInfo.src = cms.InputTag(retVal["pfTauCollectionShrinkingConeEllipticPhotonIso"])
+    setattr(process.ntupleProducer.sources, "pfTausShrinkingConeEllPhotonIso_gen", process.pfTausShrinkingConeEllipticPhotonIso_genInfo)
+    # add in information about generator level visible taus and all generator level jets
+    setattr(process.ntupleProducer.sources, "tauGenJets", process.tauGenJets_genInfo)
+    setattr(process.ntupleProducer.sources, "genJets", process.genJets_genInfo)
+    setattr(process.ntupleProducer.sources, "genPhaseSpaceEventInfo", process.genPhaseSpaceEventInfo_template)
+#--------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------
+#
+# updated InputTags for HLT trigger result object
+# in case running on reprocessed Spring'10 Monte Carlo samples
+if isSpring10:
+    process.hltMu.selector.src = cms.InputTag('TriggerResults::REDIGI')
+    process.patTrigger.processName = cms.string('REDIGI')
+    process.patCaloTausTriggerEvent.processName = cms.string('REDIGI')
+    process.patPFTausTriggerEventFixedCone.processName = cms.string('REDIGI')
+    process.patPFTausTriggerEventShrinkingCone.processName = cms.string('REDIGI')
+    process.patPFTausTriggerEventHPS.processName = cms.string('REDIGI')    
+    process.ntupleProducer.sources.trigger.src = cms.InputTag('TriggerResults::REDIGI')
+#--------------------------------------------------------------------------------    
+
+#--------------------------------------------------------------------------------
+#
+# Save ntuple
+#
+process.ntupleOutputModule = cms.OutputModule("PoolOutputModule",
+    cms.PSet(
+        outputCommands = cms.untracked.vstring(
+            "drop *",
+            "keep *_*ntupleProducer*_*_*"
+        )                               
+    ),
+    process.wPlusJetsEnrichedEventSelection, # comment-out to disable filtering of events put in Tau Ntuple                         
+    verbose = cms.untracked.bool(False),
+    fileName = cms.untracked.string("tauIdEffEDNtuple_wPlusJetsEnriched.root")      
+)
+#--------------------------------------------------------------------------------
+
+process.p = cms.Path(
+    process.prePatProductionSequence
+   + process.patTupleProductionSequence
+   #+ process.printEventContent
+   + process.ntupleProducer
+)
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
+process.o = cms.EndPath(process.ntupleOutputModule)
+
+# define order in which different paths are run
+process.schedule = cms.Schedule(
+    process.p,
+    process.muonCaloTauSkimPath,
+    process.muonPFTauSkimPath,
+    process.o
+)
+
+# print-out all python configuration parameter information
+#print process.dumpPython()
+
