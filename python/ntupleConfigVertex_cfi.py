ea0299e8d9005c0ba5d6bb72e067c8268e1cbf78
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-10-16 13:51:46 +0000

    store x,y,z coordinates of **all** primary event vertex candidates

--- python/ntupleConfigVertex_cfi.py
+++ python/ntupleConfigVertex_cfi.py
@@ -1,23 +1,25 @@
 import FWCore.ParameterSet.Config as cms
 
 #--------------------------------------------------------------------------------
-# define variables for x,y,z coordinates of reconstructed primary event vertex
+# define variables for x,y,z coordinates of reconstructed primary event vertices
 #
 # NOTE: x and y coordinates are computed with respect to beam-spot
 #
 #--------------------------------------------------------------------------------
 
 vertex_template = cms.PSet(
-    vector = cms.bool(False),
+    # Select multiplicy of object(s) to store
+    vector = cms.bool(True), # Store a value for all objects in this collection
+    #indices = cms.vuint_32([0, 1, 2]) # Store values for first, second, third objects
     
-    pluginType = cms.string("VertexValExtractor"),
+    pluginType = cms.string("VertexVectorValExtractor"),
 
     src = cms.InputTag("offlinePrimaryVertices"),
     srcBeamSpot = cms.InputTag("offlineBeamSpot"),
 
     columns = cms.PSet(
-        vertexX = cms.string("vertexX"),
-        vertexY = cms.string("vertexY"),
-        vertexZ = cms.string("vertexZ")
+        x = cms.string("vertexX"),
+        y = cms.string("vertexY"),
+        z = cms.string("vertexZ")
     )
 )    
