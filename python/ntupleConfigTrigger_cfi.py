04dcc32f1adec4a86f728315d2f3c0f3f53acf10
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-10-16 13:52:27 +0000

    add trigger prescale values

--- python/ntupleConfigTrigger_cfi.py
+++ python/ntupleConfigTrigger_cfi.py
@@ -7,17 +7,23 @@ import FWCore.ParameterSet.Config as cms
 trigger_template = cms.PSet(
     vector = cms.bool(False),
     
-    pluginType = cms.string("HLTInfoExtractor"),
+    pluginType = cms.string("PATTriggerInfoExtractor"),
 
-    src = cms.InputTag('TriggerResults::HLT'),
+    src = cms.InputTag('patTriggerEvent'),
 
     columns = cms.PSet(
-        hltL1Jet6U          = cms.string("HLT_L1Jet6U"),
-        hltJet15U           = cms.string("HLT_Jet15U"),
-        hltJet30U           = cms.string("HLT_Jet30U"),
-        hltJet50U           = cms.string("HLT_Jet50U"),
-        hltMinBiasBSC       = cms.string("HLT_MinBiasBSC"),
-        hltMinBiasBSCnoBPTX = cms.string("HLT_MinBiasBSC_NoBPTX")
+        hltL1Jet6Ubit               = cms.string("HLT_L1Jet6U:bit"),
+        hltL1Jet6Uprescale          = cms.string("HLT_L1Jet6U:prescale"),
+        hltJet15Ubit                = cms.string("HLT_Jet15U:bit"),
+        hltJet15Uprescale           = cms.string("HLT_Jet15U:prescale"),
+        hltJet30Ubit                = cms.string("HLT_Jet30U:bit"),
+        hltJet30Uprescale           = cms.string("HLT_Jet30U:prescale"),
+        hltJet50Ubit                = cms.string("HLT_Jet50U:bit"),
+        hltJet50Uprescale           = cms.string("HLT_Jet50U:prescale"),
+        hltMinBiasBSCbit            = cms.string("HLT_MinBiasBSC:bit"),
+        hltMinBiasBSCprescale       = cms.string("HLT_MinBiasBSC:prescale"),
+        hltMinBiasBSCnoBPTXbit      = cms.string("HLT_MinBiasBSC_NoBPTX:bit"),
+        hltMinBiasBSCnoBPTXprescale = cms.string("HLT_MinBiasBSC_NoBPTX:prescale")
     ),
 
     maxWarnings = cms.int32(1)
