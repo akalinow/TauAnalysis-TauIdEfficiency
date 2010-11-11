1e673a1b5aba6f0781627e201eec3efeebbe0315
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-11-11 13:57:49 +0000

    added config file for extracting number of added MC pile-up interactions

---
+++ python/ntupleConfigGenPileUpEventInfo_cfi.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+#--------------------------------------------------------------------------------
+# define variables for multiplicity of mixed-in pile-up interactions
+# added on Monte Carlo generator level 
+#--------------------------------------------------------------------------------
+
+genPileUpEventInfo_template = cms.PSet(
+    vector = cms.bool(False),
+    
+    pluginType = cms.string("GenPileUpEventInfoExtractor"),
+
+    src = cms.InputTag('addPileupInfo'),
+
+    columns = cms.PSet(
+        genPtHat = cms.string("numPileUpInteractions"),
+    )
+)    
