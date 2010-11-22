6d510fb2a24e1a39bac6d9bd4ba3cc43e5826d75
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-11-22 16:08:06 +0000

    added variables for new HPS + TaNC combined tau id. algorithm

---
+++ python/ntupleConfigPFTauHPSpTaNC_cfi.py
@@ -0,0 +1,123 @@
+import FWCore.ParameterSet.Config as cms
+
+#--------------------------------------------------------------------------------
+# define variables specific to PFTaus
+# reconstructed by HPS + TaNC combined tau id. algorithm
+#--------------------------------------------------------------------------------
+
+pfTausHPSpTaNC_recInfo = cms.PSet(
+    # Select multiplicy of object(s) to store
+    vector = cms.bool(True), # Store a value for all objects in this collection
+    #indices = cms.vuint_32([0, 1, 2]) # Store values for first, second, third objects
+    
+    # Extractor plugin
+    pluginType = cms.string("PATTauVectorValExtractor"),
+    
+    # Collection to extract from
+    src = cms.InputTag("patPFTausDijetTagAndProbeHPSpTaNC"),
+    
+    # Variables to compute for this source
+    columns = cms.PSet(
+        # kinematic variables for PFTau
+        pt = cms.string("pt()"),
+        eta = cms.string("eta()"),
+        phi = cms.string("phi()"),
+        mass = cms.string("mass()"),
+
+        # charge of PFTau
+        # (sum of charges of charged particles within signal cone)
+        charge = cms.string("charge()"),
+
+        # transverse momentum of leading charged/neutral particle within signal cone
+        leadChargedParticlePt = cms.string("? leadPFChargedHadrCand().isNonnull() ? leadPFChargedHadrCand().pt() : 0."),
+        leadNeutralParticlePt = cms.string("? leadPFNeutralCand().isNonnull() ? leadPFNeutralCand().pt() : 0."),
+        leadParticlePt = cms.string("? leadPFCand().isNonnull() ? leadPFCand().pt() : 0."),
+
+        # multiplicity and Pt sum of charged/neutral particles within signal and isolation cones
+        numChargedParticlesSignalCone = cms.string("signalPFChargedHadrCands().size()"),
+        numNeutralHadronsSignalCone = cms.string("signalPFNeutrHadrCands().size()"),
+        numPhotonsSignalCone = cms.string("signalPFGammaCands().size()"),
+        numParticlesSignalCone = cms.string("signalPFCands().size()"),
+
+        numChargedParticlesIsoCone = cms.string("isolationPFChargedHadrCands().size()"),
+        numNeutralHadronsIsoCone = cms.string("isolationPFNeutrHadrCands().size()"),
+        numPhotonsIsoCone = cms.string("isolationPFGammaCands().size()"),
+        numParticlesIsoCone = cms.string("isolationPFCands().size()"),
+
+        ptSumChargedParticlesIsoCone = cms.string("isolationPFChargedHadrCandsPtSum()"),
+        ptSumPhotonsIsoCone = cms.string("isolationPFGammaCandsEtSum()"),
+
+        # kinematic variables for PFJet associated to PFTau
+        jetPt = cms.string("pfTauTagInfoRef().pfjetRef().pt()"),
+        jetEta = cms.string("pfTauTagInfoRef().pfjetRef().eta()"),
+        jetPhi = cms.string("pfTauTagInfoRef().pfjetRef().phi()"),
+
+        # jet width
+        jetWidth = cms.string("sqrt(etaetaMoment() + phiphiMoment())"),
+
+        # flags for tag/probe and Pt_index for distinguishing between
+        # highest Pt, second highest Pt and third highest Pt jet
+        tag = cms.string("userFloat('tag')"),
+        probe = cms.string("userFloat('probe')"),
+        ptIndex = cms.string("userFloat('pt_index')"),
+
+        # tau id. discriminators based on leading track/PFChargedHadron                                                 
+        byLeadTrackFinding = cms.string("tauID('leadingTrackFinding')"),
+        byLeadTrackPtCut = cms.string("tauID('leadingTrackPtCut')"),
+        
+        # tau id. discriminators based on leading pion (PFChargedHadron or PFGamma)                                    
+        byLeadPionPtCut = cms.string("tauID('leadingPionPtCut')"),
+
+        # tau decay mode
+        decayMode = cms.string('decayMode()'),
+        
+        # additional TaNC (neural network) based discriminators
+        byTaNCraw = cms.string("tauID('byTaNCraw')"),
+        byTaNC = cms.string("tauID('byTaNC')"),
+        byTaNCloose = cms.string("tauID('byTaNCloose')"),
+        byTaNCmedium = cms.string("tauID('byTaNCmedium')"),
+        byTaNCtight = cms.string("tauID('byTaNCtight')"),        
+
+        # HPS discriminators
+        byDecayMode = cms.string("tauID('byDecayMode')"),
+        byHPSloose = cms.string("tauID('byHPSloose')"),
+        byHPSmedium = cms.string("tauID('byHPSmedium')"),
+        byHPStight = cms.string("tauID('byHPStight')"),
+        
+        # discriminators against electrons/muons
+        againstElectron = cms.string("tauID('againstElectron')"),
+        againstMuon = cms.string("tauID('againstMuon')"),
+        againstCaloMuon = cms.string("tauID('againstCaloMuon')")
+    )
+)
+
+pfTausHPSpTaNC_genInfo = pfTausHPSpTaNC_recInfo.clone(
+    pluginType = cms.string("PATTauVectorGenJetValExtractor"),
+
+    columns = cms.PSet(
+        # generator level information
+        genMatch = cms.string("genMatch"),
+        
+        genPt = cms.string("genPt"),
+        genMass = cms.string("genMass"),
+        genEta = cms.string("genEta"),
+        genPhi = cms.string("genPhi"),
+
+        genDecayMode = cms.string("genDecayMode"),
+        
+        genPdgId = cms.string("genPdgId")
+    ),
+    
+    srcGenParticles = cms.InputTag('genParticles'),
+    skipPdgIdsGenParticleMatch = cms.vint32( 12, 14, 16 )
+)    
+
+pfTausHPSpTaNC_mcEmbeddingInfo = pfTausHPSpTaNC_recInfo.clone(
+    pluginType = cms.string("PATTauVectorValExtractor"),
+
+    columns = cms.PSet(
+        # flags for tag/probe
+        tag = cms.string("userFloat('tag')"),
+        probe = cms.string("userFloat('probe')"),
+    )
+)    
