75a98a95b8d0064b8e2260f1c320c9e950926242
Sho Maruyama <sha1-b47564b910de3cc3cd657a4905f52d93b10eea83@cern.ch>
2010-11-30 09:53:00 +0000

    matched to sample.py data names plus dataset name definition at the beginning of the file

--- test/commissioning/plotTauFakeRate.py
+++ test/commissioning/plotTauFakeRate.py
@@ -3,9 +3,13 @@
 #
 # This Macro assumes:
 # mySamples.py exists and
-# data2, ppMuX, minBias, qcd dataset  
+# data_ppmux_runs132440to145761, ppmux_mc, data_dijet_runs132440to135802, qcddijet_mc dataset  
 # are defined there
 #
+gDiJetData = "qcdDiJet_data_runs132440to135802" 
+gDiJetMC = "mc_qcddijet" 
+gPPmuXData = "qcdMuEnriched_data_runs132440to145761" 
+gPPmuXMC = "mc_ppmux" 
 
 import ROOT
 
@@ -144,14 +148,14 @@ def makeJetEffPlot(expression, denominator,numerator,
     eff_result['background'].GetYaxis().SetTitleOffset(1.2)
     eff_result['background'].GetYaxis().SetTitleSize(0.05) 
     eff_result['background'].GetYaxis().SetLabelSize(0.05)
-  
+ 
 ### Why is this needed ???
     eff_result['background'].GetYaxis().SetTitle("Efficiency" ) 
     eff_result['background'].Draw() 
-    eff_result['samples']['data2'].Draw('ep same') 
-    eff_result['samples']['qcd'].Draw('ep same') 
-    eff_result['samples']['ppMuX'].Draw('ep same') 
-    eff_result['samples']['minBias'].Draw('ep same') 
+    eff_result['samples'][gPPmuXData].Draw('ep same') 
+    eff_result['samples'][gDiJetMC].Draw('ep same') 
+    eff_result['samples'][gPPmuXMC].Draw('ep same') 
+    eff_result['samples'][gDiJetData].Draw('ep same') 
 ### Fix it later!
 
     # Draw the legend - you can pass NDC xl, yl, xh, yh coordinates to make_legend(...)
@@ -180,32 +184,32 @@ def makeJetEffPlot(expression, denominator,numerator,
     bottomPad_diff.Draw()
     bottomPad_diff.cd()
 
-    h_data = plot2.draw(list(plotter.samples['data2']['sample'].events_and_weights(None)),expression, numerator, "data2Num", binning=binning ) # muon stream data
-    h_dataDen = plot2.draw(list(plotter.samples['data2']['sample'].events_and_weights(None)),expression, denominator, "data2Den", binning=binning )
+    h_data = plot2.draw(list(plotter.samples[gPPmuXData]['sample'].events_and_weights(None)),expression, numerator, gPPmuXData+"Num", binning=binning ) # muon stream data
+    h_dataDen = plot2.draw(list(plotter.samples[gPPmuXData]['sample'].events_and_weights(None)),expression, denominator, gPPmuXData+"Den", binning=binning )
     h_data.Divide(h_dataDen)
-    h_mc = plot2.draw(list(plotter.samples['ppMuX']['sample'].events_and_weights(None)),expression, numerator, "ppMuXNum", binning=binning )
-    h_mcDen = plot2.draw(list(plotter.samples['ppMuX']['sample'].events_and_weights(None)),expression, denominator, "ppMuXDen", binning=binning )
+    h_mc = plot2.draw(list(plotter.samples[gPPmuXMC]['sample'].events_and_weights(None)),expression, numerator, gPPmuXMC+"Num", binning=binning )
+    h_mcDen = plot2.draw(list(plotter.samples[gPPmuXMC]['sample'].events_and_weights(None)),expression, denominator, gPPmuXMC+"Den", binning=binning )
     h_mc.Divide(h_mcDen)
-    h_mc0 = plot2.draw(list(plotter.samples['minBias']['sample'].events_and_weights(None)),expression, numerator, "minBiasNum", binning=binning )
-    h_mc0Den = plot2.draw(list(plotter.samples['minBias']['sample'].events_and_weights(None)),expression, denominator, "minBiasDen", binning=binning )
+    h_mc0 = plot2.draw(list(plotter.samples[gDiJetData]['sample'].events_and_weights(None)),expression, numerator, gDiJetData+"Num", binning=binning )
+    h_mc0Den = plot2.draw(list(plotter.samples[gDiJetData]['sample'].events_and_weights(None)),expression, denominator, gDiJetData+"Den", binning=binning )
     h_mc0.Divide(h_mc0Den)
-    h_mc2 = plot2.draw(list(plotter.samples['qcd']['sample'].events_and_weights(None)),expression, numerator, "qcdNum", binning=binning )
-    h_mc2Den = plot2.draw(list(plotter.samples['qcd']['sample'].events_and_weights(None)),expression, denominator, "qcdDen", binning=binning )
+    h_mc2 = plot2.draw(list(plotter.samples[gDiJetMC]['sample'].events_and_weights(None)),expression, numerator, gDiJetMC+"Num", binning=binning )
+    h_mc2Den = plot2.draw(list(plotter.samples[gDiJetMC]['sample'].events_and_weights(None)),expression, denominator, gDiJetMC+"Den", binning=binning )
     h_mc2.Divide(h_mc2Den)
 
 # muon stream and ppmux
-    diff_ppMuX = plot_ratio_deviations(h_data, h_mc)
-    style.update_histo_style(diff_ppMuX['diff'], style.QCD_MC_PYTHIA8_STYLE_HIST)
-    diff_ppMuX['diff'].SetTitle(";%s;#frac{#Delta(Data,MC)  }{MC};"%(x_axis_title))
-    diff_ppMuX['diff'].Draw("")
+    diff_ppmux_mc = plot_ratio_deviations(h_data, h_mc)
+    style.update_histo_style(diff_ppmux_mc['diff'], style.QCD_MC_PYTHIA8_STYLE_HIST)
+    diff_ppmux_mc['diff'].SetTitle(";%s;#frac{#Delta(Data,MC)  }{MC};"%(x_axis_title))
+    diff_ppmux_mc['diff'].Draw("")
 
-    diff_minBias = plot_ratio_deviations(h_mc0, h_mc2)
-    style.update_histo_style(diff_minBias['diff'], style.QCD_MC_PYTHIA6_STYLE_HIST)
-    diff_minBias['diff'].Draw("same")
+    diff_data_dijet_runs132440to135802 = plot_ratio_deviations(h_mc0, h_mc2)
+    style.update_histo_style(diff_data_dijet_runs132440to135802['diff'], style.QCD_MC_PYTHIA6_STYLE_HIST)
+    diff_data_dijet_runs132440to135802['diff'].Draw("same")
 
     leg2 = ROOT.TLegend(0.8,0.38,0.94,0.5);
-    leg2.AddEntry(diff_ppMuX['diff'],"Muon,ppMuX  ","lep");
-    leg2.AddEntry(diff_minBias['diff'],"JetMETTau,QCD  ","lep");
+    leg2.AddEntry(diff_ppmux_mc['diff'],"Muon,ppmux_mc  ","lep");
+    leg2.AddEntry(diff_data_dijet_runs132440to135802['diff'],"JetMETTau,QCD  ","lep");
     leg2.SetFillStyle(0);
     leg2.Draw();
     canvas_diff.cd()
@@ -255,16 +259,16 @@ if __name__ == "__main__":
     plotter = PlotManager()
 
     # Add each sample we want to plot/compare
-    plotter.add_sample(mySamples.data2, "Muon Data(L=1.08pb^{-1})   ", **style.DATA_STYLE)
-    plotter.add_sample(mySamples.ppMuX_mc, "PPMuX MC", **style.QCD_MC_PYTHIA6_STYLE_HIST)
-    plotter.add_sample(mySamples.minBias_mc, "JetMETTau(Data L=12.8nb^{-1})   ", **style.MINBIAS_MC_STYLE )
-    plotter.add_sample(mySamples.qcd, "QCD MC", **style.QCD_MC_PYTHIA8_STYLE_HIST)
+    plotter.add_sample(mySamples.data_ppmux_runs132440to145761, "Muon Data   ", **style.DATA_STYLE)
+    plotter.add_sample(mySamples.ppmux_mc, "PPMuX MC", **style.QCD_MC_PYTHIA6_STYLE_HIST)
+    plotter.add_sample(mySamples.data_dijet_runs132440to135802, "JetMETTau Data   ", **style.MINBIAS_MC_STYLE )
+    plotter.add_sample(mySamples.qcddijet_mc, "QCD MC", **style.QCD_MC_PYTHIA8_STYLE_HIST)
 
     # Normalize everything to the data luminosity
-    plotter.set_integrated_lumi(mySamples.data2.effective_luminosity())
+    plotter.set_integrated_lumi(mySamples.data_ppmux_runs132440to145761.effective_luminosity())
 
     # Build the ntuple manager
-    ntuple_manager = mySamples.data2.build_ntuple_manager("tauIdEffNtuple")
+    ntuple_manager = mySamples.data_ppmux_runs132440to145761.build_ntuple_manager("tauIdEffNtuple")
 
     # Get HLT trigger decisions
 #    hlt = ntuple_manager.get_ntuple("TriggerResults")
