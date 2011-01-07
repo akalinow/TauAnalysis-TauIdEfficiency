e7ce144c83183ac89be1b7f96b2dc521df0b0633
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-01-07 11:36:50 +0000

    - added separate plots for dR = 0.4/0.6 isolation cones - added plots of loosePFIsoPtSum before/after TaNC/HPS tau id. discriminators are applied

--- test/commissioning/pfLooseIsolationPlots.py
+++ test/commissioning/pfLooseIsolationPlots.py
@@ -21,6 +21,82 @@ import copy
 maxNumEntries = 1000000000
 #maxNumEntries = 10000
 
+def makeLoosePFIsoPlot(ztt_events,
+                       genSelection_e, genSelection_mu, genSelection_had,
+                       recSelection, recExpression,
+                       outputFileName):
+    canvas = ROOT.TCanvas("canvas", "canvas", 500, 500)
+    canvas.SetLeftMargin(0.11)
+    canvas.SetGridy(1)
+
+    histogram_e = None
+    if genSelection_e is not None:
+        histogram_e = draw(
+            ztt_events,
+            expression = recExpression,
+            selection = genSelection_e & recSelection,
+            binning = (20, 0, 10),
+            output_name = 'e',
+            maxNumEntries = maxNumEntries
+        )
+        histogram_e.Sumw2()
+        histogram_e.Scale(1./histogram_e.Integral())
+        histogram_e.SetMarkerSize(2)
+        histogram_e.SetMarkerStyle(20)
+        histogram_e.SetMarkerColor(3)
+        histogram_e.SetLineColor(3)
+        histogram_e.Draw("e1p")
+
+    histogram_mu = None
+    if genSelection_mu is not None:
+        histogram_mu = draw(
+            ztt_events,
+            expression = recExpression,
+            selection = genSelection_mu & recSelection,
+            binning = (20, 0, 10),
+            output_name = 'mu',
+            maxNumEntries = maxNumEntries
+        )
+        histogram_mu.Sumw2()
+        histogram_mu.Scale(1./histogram_mu.Integral())
+        histogram_mu.SetMarkerSize(2)
+        histogram_mu.SetMarkerStyle(20)
+        histogram_mu.SetMarkerColor(4)
+        histogram_mu.SetLineColor(4)
+        histogram_mu.Draw("e1psame")
+
+    histogram_had = None
+    if genSelection_had is not None:
+        histogram_had = draw(
+            ztt_events,
+            expression = recExpression,
+            selection = genSelection_had & recSelection,
+            binning = (20, 0, 10),
+            output_name = 'had',
+            maxNumEntries = maxNumEntries
+        )
+        histogram_had.Sumw2()
+        histogram_had.Scale(1./histogram_had.Integral())
+        histogram_had.SetMarkerSize(2)
+        histogram_had.SetMarkerStyle(20)
+        histogram_had.SetMarkerColor(2)
+        histogram_had.SetLineColor(2)
+        histogram_had.Draw("e1psame")
+
+    # Draw the legend - you can pass NDC xl, yl, xh, yh coordinates to make_legend(...)
+    legend = ROOT.TLegend(0.60, 0.69, 0.88, 0.88)
+    if histogram_e is not None:
+        legend.AddEntry(histogram_e, "#tau #rightarrow e", "p")
+    if histogram_mu is not None:     
+        legend.AddEntry(histogram_mu, "#tau #rightarrow #mu", "p")
+    if histogram_had is not None: 
+        legend.AddEntry(histogram_had, "#tau #rightarrow had.", "p")
+    legend.Draw()
+    
+    canvas.SaveAs(outputFileName[:outputFileName.rfind(".")] + ".png")
+    canvas.SaveAs(outputFileName[:outputFileName.rfind(".")] + ".pdf")
+    canvas.SaveAs(outputFileName[:outputFileName.rfind(".")] + ".root")
+    
 if __name__ == "__main__":
     ROOT.gROOT.SetBatch(True)
     ROOT.gROOT.SetStyle("Plain")
@@ -34,76 +110,81 @@ if __name__ == "__main__":
     ntuple_manager = ztt.build_ntuple_manager("tauIdEffNtuple")
 
     genTaus = ntuple_manager.get_ntuple("tauGenJets")
-    genSelection_e = genTaus.expr('$genDecayMode == 0 && $genPt > 10 && abs($genEta) < 2.1')
-    genSelection_mu = genTaus.expr('$genDecayMode == 1 && $genPt > 10 && abs($genEta) < 2.1')
-    genSelection_had = genTaus.expr('$genDecayMode > 1.5 && $genPt > 10 && abs($genEta) < 2.1')
+    genSelection_e   = genTaus.expr('$genDecayMode == 0   && $genPt > 10 && abs($genEta) < 2.1')
+    genSelection_mu  = genTaus.expr('$genDecayMode == 1   && $genPt > 10 && abs($genEta) < 2.1')
+    genSelection_had = genTaus.expr('$genDecayMode >  1.5 && $genPt > 10 && abs($genEta) < 2.1')
 
     recTaus = ntuple_manager.get_ntuple("hpstanc")
-    recSelection = recTaus.expr('$byLeadTrackFinding > 0.5 && $byLeadTrackPtCut > 0.5')
-
-    recExpression = recTaus.expr('$ptSumLooseIsolation')
+    #accSelection                = recTaus.expr('$pt > 20 && abs($eta) < 2.3')
+    accSelection                = recTaus.expr('$pt > 20 && abs($eta) < 2.1')
+    recSelectionBeforeTauId     = accSelection            & recTaus.expr('$byLeadTrackFinding > 0.5 && $byLeadTrackPtCut > 0.5')
+    recSelectionAfterTaNCloose  = recSelectionBeforeTauId & recTaus.expr('$byTaNCloose  > 0.5')
+    recSelectionAfterTaNCmedium = recSelectionBeforeTauId & recTaus.expr('$byTaNCmedium > 0.5')
+    recSelectionAfterTaNCtight  = recSelectionBeforeTauId & recTaus.expr('$byTaNCtight  > 0.5')
+    recSelectionAfterHPSloose   = recSelectionBeforeTauId & recTaus.expr('$byHPSloose   > 0.5')
+    recSelectionAfterHPSmedium  = recSelectionBeforeTauId & recTaus.expr('$byHPSmedium  > 0.5')
+    recSelectionAfterHPStight   = recSelectionBeforeTauId & recTaus.expr('$byHPStight   > 0.5')
+    
+    recExpressionLoosePFIso04 = recTaus.expr('$ptSumLooseIsolation04')
+    recExpressionLoosePFIso06 = recTaus.expr('$ptSumLooseIsolation06')
 
     ztt_events = list(ztt.events_and_weights())[0][0]
 
-    canvas = ROOT.TCanvas("canvas", "canvas", 500, 500)
-    canvas.SetLeftMargin(0.11)
-    canvas.SetGridy(1)
+    makeLoosePFIsoPlot(ztt_events,
+                       genSelection_e, genSelection_mu, genSelection_had,
+                       recSelectionBeforeTauId, recExpressionLoosePFIso04,
+                       "./plots/pfLooseIsolation04_beforeTauId.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       genSelection_e, genSelection_mu, genSelection_had,
+                       recSelectionBeforeTauId, recExpressionLoosePFIso06,
+                       "./plots/pfLooseIsolation06_beforeTauId.png")
 
-    histogram_e = draw(
-        ztt_events,
-        expression = recExpression,
-        selection = genSelection_e & recSelection,
-        binning = (20, 0, 10),
-        output_name = 'e',
-        maxNumEntries = maxNumEntries
-    )
-    histogram_e.Sumw2()
-    histogram_e.Scale(1./histogram_e.Integral())
-    histogram_e.SetMarkerSize(2)
-    histogram_e.SetMarkerStyle(20)
-    histogram_e.SetMarkerColor(3)
-    histogram_e.SetLineColor(3)
-    histogram_e.Draw("e1p")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterTaNCloose, recExpressionLoosePFIso04,
+                       "./plots/pfLooseIsolation04_afterTaNCloose.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterTaNCloose, recExpressionLoosePFIso06,
+                       "./plots/pfLooseIsolation06_afterTaNCloose.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterTaNCmedium, recExpressionLoosePFIso04,
+                       "./plots/pfLooseIsolation04_afterTaNCmedium.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterTaNCmedium, recExpressionLoosePFIso06,
+                       "./plots/pfLooseIsolation06_afterTaNCmedium.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterTaNCtight, recExpressionLoosePFIso04,
+                       "./plots/pfLooseIsolation04_afterTaNCtight.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterTaNCtight, recExpressionLoosePFIso06,
+                       "./plots/pfLooseIsolation06_afterTaNCtight.png")
 
-    histogram_mu = draw(
-        ztt_events,
-        expression = recExpression,
-        selection = genSelection_mu & recSelection,
-        binning = (20, 0, 10),
-        output_name = 'mu',
-        maxNumEntries = maxNumEntries
-    )
-    histogram_mu.Sumw2()
-    histogram_mu.Scale(1./histogram_mu.Integral())
-    histogram_mu.SetMarkerSize(2)
-    histogram_mu.SetMarkerStyle(20)
-    histogram_mu.SetMarkerColor(4)
-    histogram_mu.SetLineColor(4)
-    histogram_mu.Draw("e1psame")
-
-    histogram_had = draw(
-        ztt_events,
-        expression = recExpression,
-        selection = genSelection_had & recSelection,
-        binning = (20, 0, 10),
-        output_name = 'had',
-        maxNumEntries = maxNumEntries
-    )
-    histogram_had.Sumw2()
-    histogram_had.Scale(1./histogram_had.Integral())
-    histogram_had.SetMarkerSize(2)
-    histogram_had.SetMarkerStyle(20)
-    histogram_had.SetMarkerColor(2)
-    histogram_had.SetLineColor(2)
-    histogram_had.Draw("e1psame")
-
-    # Draw the legend - you can pass NDC xl, yl, xh, yh coordinates to make_legend(...)
-    legend = ROOT.TLegend(0.60, 0.69, 0.88, 0.88)
-    legend.AddEntry(histogram_e, "#tau #rightarrow e", "p")
-    legend.AddEntry(histogram_mu, "#tau #rightarrow #mu", "p")
-    legend.AddEntry(histogram_had, "#tau #rightarrow had.", "p")
-    legend.Draw()
-    
-    canvas.SaveAs("./plots/pfLooseIsolation.png")
-    canvas.SaveAs("./plots/pfLooseIsolation.pdf")
-        
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterHPSloose, recExpressionLoosePFIso04,
+                       "./plots/pfLooseIsolation04_afterHPSloose.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterHPSloose, recExpressionLoosePFIso06,
+                       "./plots/pfLooseIsolation06_afterHPSloose.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterHPSmedium, recExpressionLoosePFIso04,
+                       "./plots/pfLooseIsolation04_afterHPSmedium.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterHPSmedium, recExpressionLoosePFIso06,
+                       "./plots/pfLooseIsolation06_afterHPSmedium.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterHPStight, recExpressionLoosePFIso04,
+                       "./plots/pfLooseIsolation04_afterHPStight.png")
+    makeLoosePFIsoPlot(ztt_events,
+                       None, None, genSelection_had,
+                       recSelectionAfterHPStight, recExpressionLoosePFIso06,
+                       "./plots/pfLooseIsolation06_afterHPStight.png")
