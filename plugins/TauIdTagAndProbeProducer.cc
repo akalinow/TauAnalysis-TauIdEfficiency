3c7edb9ff6120c5430c8b7ebeed2bf38ec968514
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-02-10 13:42:43 +0000

    added suport for multiple triggers paths

--- plugins/TauIdTagAndProbeProducer.cc
+++ plugins/TauIdTagAndProbeProducer.cc
@@ -14,19 +14,18 @@
  *
  */
 
-using namespace edm;
-using namespace std;
+typedef std::vector<std::string> vstring;
 
-class TauIdTagAndProbeProducer : public EDProducer 
+class TauIdTagAndProbeProducer : public edm::EDProducer 
 {  
  public:
   struct TauInfo 
   {
     TauInfo()
-      : tau_(0), tauPt_(0.), matchesTriggerObject_(false)
+      : tau_(0), tauPt_(0.)
     {}
-    TauInfo(const pat::Tau& tau, bool matchesTriggerObject)
-      : tau_(&tau), matchesTriggerObject_(matchesTriggerObject)
+    TauInfo(const pat::Tau& tau)
+      : tau_(&tau)
     {
       // get Pt of jet associated to tau
       if ( tau.isPFTau() ) {
@@ -39,23 +38,23 @@ class TauIdTagAndProbeProducer : public EDProducer
     }
     const pat::Tau* tau_;
     double tauPt_;
-    bool matchesTriggerObject_;
+    std::map<std::string, bool> matchesTriggerObject_;
   };
   
   typedef std::vector<pat::Tau> vPatTaus;
-  explicit TauIdTagAndProbeProducer(const ParameterSet& pset);
+  explicit TauIdTagAndProbeProducer(const edm::ParameterSet& pset);
   virtual ~TauIdTagAndProbeProducer(){}
-  void produce(Event&, const EventSetup&);
+  void produce(edm::Event&, const edm::EventSetup&);
   
 private:
-  InputTag src_;
-  string triggerPath_;
+  edm::InputTag src_;
+  vstring triggerPaths_;
 };
 
-TauIdTagAndProbeProducer::TauIdTagAndProbeProducer(const ParameterSet& pset)
+TauIdTagAndProbeProducer::TauIdTagAndProbeProducer(const edm::ParameterSet& pset)
 {
-  src_ = pset.getParameter<InputTag>("source");
-  triggerPath_ = pset.getParameter<string>("triggerPath");
+  src_ = pset.getParameter<edm::InputTag>("source");
+  triggerPaths_ = pset.getParameter<vstring>("triggerPaths");
   
   // register products
   produces<vPatTaus>();
@@ -71,25 +70,32 @@ namespace {
 }
 
 void
-TauIdTagAndProbeProducer::produce(Event &evt, const EventSetup &es)
+TauIdTagAndProbeProducer::produce(edm::Event& evt, const edm::EventSetup& es)
 {
   // output products
-  auto_ptr<vPatTaus> output(new vPatTaus());
+  std::auto_ptr<vPatTaus> output(new vPatTaus());
   
-  Handle<View<pat::Tau> > sourceView;
+  edm::Handle<edm::View<pat::Tau> > sourceView;
   evt.getByLabel(src_, sourceView);
   
   size_t inputSize = sourceView->size();
-  vector<TauInfo> tauInfos(inputSize);
+  std::vector<TauInfo> tauInfos(inputSize);
   // count how many taus are matched to a trigger object
-  unsigned int nTriggers = 0;
+  std::map<std::string, unsigned> nTriggers;
   for ( size_t iTau = 0; iTau < inputSize; ++iTau ){
     // get tau and check if it matches trigger
     const pat::Tau& tau = sourceView->at(iTau);
-    bool matchesTriggerObject = tau.triggerObjectMatchesByPath(triggerPath_).size();
-    TauInfo myTauInfo(tau, matchesTriggerObject);
+
+    TauInfo myTauInfo(tau);
+
+    for ( vstring::const_iterator triggerPath = triggerPaths_.begin();
+	  triggerPath != triggerPaths_.end(); ++triggerPath ) {
+      bool matchesTriggerObject = tau.triggerObjectMatchesByPath(*triggerPath).size();
+      myTauInfo.matchesTriggerObject_[*triggerPath] = matchesTriggerObject;
+      if ( matchesTriggerObject ) ++nTriggers[*triggerPath];
+    }
+
     tauInfos[iTau] = myTauInfo;
-    if ( matchesTriggerObject ) ++nTriggers;
   }
   
   // sort by descending pt
@@ -101,22 +107,36 @@ TauIdTagAndProbeProducer::produce(Event &evt, const EventSetup &es)
     // store pt-ordered index
     newTau.addUserFloat("pt_index", iTau);
     
-    // set probe flag
-    bool isProbeTau = false;
-    // At least two triggers, so this is always a probe
-    if ( nTriggers > 1 ){
-      isProbeTau = true;
+    for ( vstring::const_iterator triggerPath = triggerPaths_.begin();
+	  triggerPath != triggerPaths_.end(); ++triggerPath ) {
+
+      std::cout << "triggerPath = " << (*triggerPath) << std::endl;
+
+      // set probe flag
+      bool isProbeTau = false;
+      if ( nTriggers[*triggerPath] > 1 ){
+	// At least two triggers, so this is always a probe
+	isProbeTau = true;
+      } else if ( nTriggers[*triggerPath] == 1 && !tauInfos[iTau].matchesTriggerObject_[*triggerPath] ) {
+	// If there is only one trigger, but it is not this tau, then 
+	// this is a probe object
+	isProbeTau = true;
+      }
+
+      std::string triggerLabel;
+      if   ( triggerPath->rfind("_") == std::string::npos ) triggerLabel = (*triggerPath);
+      else triggerLabel = std::string(*triggerPath, triggerPath->rfind("_") + 1);
+      
+      std::string probeLabel = std::string("probe").append(triggerLabel);
+      std::cout << "probeLabel = " << probeLabel << std::endl;
+      newTau.addUserFloat(probeLabel.data(), isProbeTau);
+      
+      // set tag flag
+      std::string tagLabel = std::string("tag").append(triggerLabel);
+      std::cout << "tagLabel = " << tagLabel << std::endl;
+      newTau.addUserFloat(tagLabel.data(), tauInfos[iTau].matchesTriggerObject_[*triggerPath]);      
     }
-    // If there is only one trigger, but it is not this tau, then 
-    // this is a probe object
-    else if (nTriggers == 1 && !tauInfos[iTau].matchesTriggerObject_) {
-      isProbeTau = true;
-    }
-    newTau.addUserFloat("probe", isProbeTau);
-    
-    // set tag flag
-    newTau.addUserFloat("tag", tauInfos[iTau].matchesTriggerObject_);
-    
+
     // copy to output collection
     output->push_back(newTau);
   }
