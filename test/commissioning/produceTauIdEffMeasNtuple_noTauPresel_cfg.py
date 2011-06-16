0bc4add7e65f54595320c3ce0f783d8e3293a4bd
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-06-16 16:37:21 +0000

    added config files for producing PAT-tuple for tau id. efficiency measurement with no pat::Tau selection applied (other than very loose Pt > 10 GeV && |eta| < 2.5 cuts)

---
+++ test/commissioning/produceTauIdEffMeasNtuple_noTauPresel_cfg.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+from TauAnalysis.TauIdEfficiency.produceTauIdEffMeasNtuple_template_cfg import *
+
+for processAttrName in dir(process):
+    processAttr = getattr(process, processAttrName)
+    if isinstance(processAttr, cms.EDFilter):
+        if processAttr.type_() == "PATTauSelector":
+            print "--> Disabling cut %s" % processAttrName
+            setattr(processAttr, "cut", cms.string('pt > 10. & abs(eta) < 2.5'))
+
+processDumpFile = open('produceTauIdEffMeasNtuple_noTauPresel.dump' , 'w')
+print >> processDumpFile, process.dumpPython()
