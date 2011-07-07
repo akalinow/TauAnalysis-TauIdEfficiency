5407aa6d11ec404703e070e57ff3bf8274b54583
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2011-07-07 12:09:27 +0000

    updated file paths

--- test/commissioning/copyTauIdEffNtuples.py
+++ test/commissioning/copyTauIdEffNtuples.py
@@ -7,17 +7,18 @@ import subprocess
 import shlex
 
 # Get all the skim files from the castor directory
-sourceFilePath = "/castor/cern.ch/user/v/veelken/CMSSW_4_1_x/ntuples/TauIdEffMeas/" # Christian's Ntuples
+#sourceFilePath = "/castor/cern.ch/user/v/veelken/CMSSW_4_2_x/ntuples/TauIdEffMeas/" # Christian's Ntuples
+sourceFilePath = "/castor/cern.ch/user/v/veelken/CMSSW_4_2_x/PATtuples/TauIdEffMeas/" # Christian's PAT-tuples
 #sourceFilePath = "/castor/cern.ch/user/m/mverzett/tagprobe/Jun06Skim/edntuples_v3/" # Mauro's Ntuples
 source_files = [ file_info['path'] for file_info in castor.nslsl(sourceFilePath) ]
 #print "source_files:"
 #print source_files
 
 #targetFilePath = "/tmp/veelken/"
-targetFilePath = "/data2/veelken/CMSSW_4_1_x/ntuples/TauIdEffMeas/2011Jun18/"
+targetFilePath = "/data2/veelken/CMSSW_4_2_x/PATtuples/TauIdEffMeas/2011Jul01_mauro/"
 
-jobId = "2011Jun18" # Christian's Ntuples
-version = "V1"
+jobId = "2011Jul01_mauro" # Christian's Ntuples
+version = "V2"
 #jobId = "2011Jun06" # Mauro's Ntuples
 #version = "V2"
 
