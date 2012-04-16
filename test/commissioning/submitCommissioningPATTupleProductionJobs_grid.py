437008b0b08564dad7df60b6e31f23521b6f82af
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2012-04-16 11:52:54 +0000

    update PAT-tuple version to v2_2

--- test/commissioning/submitCommissioningPATTupleProductionJobs_grid.py
+++ test/commissioning/submitCommissioningPATTupleProductionJobs_grid.py
@@ -17,7 +17,7 @@ crabFilePath = "/tmp/veelken/crab/"
 pfCandidateCollection = "particleFlow" # pile-up removal disabled
 #pfCandidateCollection = "pfNoPileUp"   # pile-up removal enabled
 
-version = 'patV2_1'
+version = 'patV2_2'
 
 crab_template = """
 [CRAB]
