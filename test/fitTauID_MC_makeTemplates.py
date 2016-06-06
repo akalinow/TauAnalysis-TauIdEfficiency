b7c6ff203c7b236d832bdf4dbd6876957b8db78e
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2016-06-06 14:57:34 +0200

    * cfg.py for making the templete shepes for Z

---
+++ test/fitTauID_MC_makeTemplates.py
@@ -0,0 +1,123 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TagProbe")
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.source = cms.Source("EmptySource")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+
+filePath = "/home/akalinow/scratch/CMS/TauID/Crab/Data/TauID_TnP/DYJetsToLL_M_50_TuneCUETP8M1_13TeV_amcatnloFXFX_pythia8_v11_ext4/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/DYJetsToLL_M_50_TuneCUETP8M1_13TeV_amcatnloFXFX_pythia8_v11_ext4/160520_133905/0000/"
+
+#filePath = "./"
+filePath += "tnpZ_MC.root"
+
+
+efficiencyPSetTemplate = cms.PSet(
+    UnbinnedVariables = cms.vstring("mass"),
+    EfficiencyCategoryAndState = cms.vstring("againstMuonLoose3", "pass"), ## Numerator definition
+    BinnedVariables = cms.PSet(
+        ## Binning in continuous variables
+        abseta = cms.vdouble(0.0, 1.2, 1.7, 2.3),
+        ## flags and conditions required at the denominator,
+    ),
+    BinToPDFmap = cms.vstring("Zmumu_Model"), ## PDF to use, as defined below
+)
+
+againstMuonLoose3_Zmumu = efficiencyPSetTemplate.clone()
+againstMuonLoose3_Zmumu.BinnedVariables._Parameterizable__addParameter("mcTrue",cms.vdouble(0.5,1.0))
+againstMuonLoose3_Zmumu.BinToPDFmap = cms.vstring("Zmumu_Model_Eta0","*abseta_bin1*","Zmumu_Model_Eta1", "*abseta_bin2*","Zmumu_Model_Eta2")
+
+againstMuonLoose3_Ztautau = efficiencyPSetTemplate.clone()
+againstMuonLoose3_Ztautau.BinnedVariables._Parameterizable__addParameter("mcTrue",cms.vdouble(0,0.4))
+againstMuonLoose3_Ztautau.BinToPDFmap = cms.vstring("Ztautau_Model_Eta0","*abseta_bin1*","Ztautau_Model_Eta1", "*abseta_bin2*","Ztautau_Model_Eta2")
+
+
+Zmumu_Model = cms.vstring(
+    "Voigtian::signal1Fail(mass, mean1Fail[92, 85,95], width[2.495], sigma1Fail[1, 0.5,2])",
+    "Voigtian::signal2Fail(mass, mean2Fail[92, 85,95], width[2.495], sigma2Fail[3, 2,5])",
+    "SUM::signalFail(vFracFail[0.9, 0,1]*signal1Fail, signal2Fail)",
+    
+    "Voigtian::signal1Pass(mass, mean1Pass[92, 85,95], width[2.495], sigma1Pass[2, 1,5])",
+    "CBShape::signal2Pass(mass, mean2Pass[92, 80,95], sigma2Pass[5, 2, 10], alpha[3, 1,5], n[2, 0,5])",
+    "SUM::signalPass(vFracPass[0.9, 0,1]*signal1Pass, signal2Pass)",
+    
+    "Exponential::backgroundPass1(mass, lp1[-0.033])",
+    "SUM::backgroundPass(0*backgroundPass1)",
+    
+    "Exponential::backgroundFail1(mass, lf[-0.039])",
+    "SUM::backgroundFail(0*backgroundFail1)",
+    
+    "efficiency[0.001,0,0.01]",
+    "signalFractionInPassing[0.9]"
+)
+
+Ztautau_Model = cms.vstring(
+    "Voigtian::signal1Fail(mass, mean1Fail[90.95], width[2.495], sigma1Fail[1.32])",
+    "SUM::signalFail(0*signal1Fail)",
+    
+    "Voigtian::signal1Pass(mass, mean1Pass[90.96], width[2.495], sigma1Pass[1.3])",
+    "SUM::signalPass(0*signal1Pass)",
+    
+    "Exponential::backgroundPass1(mass, lp1[-0.033, -1,0.1])",
+    "Exponential::backgroundPass2(mass, lp2[-0.168, -1,0.1])",            
+    "SUM::backgroundZtautauPass(vFracBkgPass[0.2, 0,1]*backgroundPass1, backgroundPass2)",
+    "SUM::backgroundPass(backgroundZtautauPass)",
+    
+    "Exponential::backgroundFail1(mass, lf[-0.039, -1, 0.1])",
+    "SUM::backgroundFail(backgroundFail1)",
+    
+    "efficiency[0.001,0,0.01]",
+    "signalFractionInPassing[0.9]"
+)
+###############################################################
+###############################################################
+process.TnP_Muon_ID = cms.EDAnalyzer("TagProbeFitTreeAnalyzer",
+    ## Input, output
+    InputFileNames = cms.vstring("file:"+filePath),
+    OutputFileName = cms.string("TnP_MuonToTau_MisID_MC_Templates.root"),
+    InputTreeName = cms.string("fitter_tree"), 
+    InputDirectoryName = cms.string("tpTree"),  
+    ## Variables for binning
+    Variables = cms.PSet(
+        mass   = cms.vstring("Tag-muon Mass", "70", "110", "GeV/c^{2}"),
+        abseta = cms.vstring("muon |#eta|", "0", "2.4", ""),
+        tag_triggerMatch = cms.vstring("Tag matched to HLT item", "0.5", "1.0", ""),
+        pair_dz = cms.vstring("#Deltaz between two muons", "-0.05", "0.05", "cm"),
+        pair_deltaR = cms.vstring("#DeltaR between two muons", "0.5", "5", ""),
+        pair_probeMultiplicity = cms.vstring("Probe multiplicity", "1", "1", ""),
+        decayModeFinding = cms.vstring("Decay mode finding", "0.5", "1.0", ""),
+        #decayModeFindingNewDMs = cms.vstring("Decay mode finding NewDMs", "0.5", "1.0", ""),
+        byLooseCombinedIsolationDeltaBetaCorr3Hits = cms.vstring("Combined loose isolation", "0.5", "1.0", ""),
+        mcTrue = cms.vstring("Match to gen muons", "0.0", "1.0", ""),
+    ),
+    ## Flags you want to use to define numerator and possibly denominator
+    Categories = cms.PSet(
+        againstMuonTight3 = cms.vstring("againstMuonTight3", "dummy[pass=1,fail=0]"),
+        againstMuonLoose3 = cms.vstring("againstMuonLoose3", "dummy[pass=1,fail=0]"),
+    ),
+    ## What to fit
+    Efficiencies = cms.PSet(
+        againstMuonLoose3_Zmumu = againstMuonLoose3_Zmumu,
+        againstMuonLoose3_Ztautau = againstMuonLoose3_Ztautau,
+    ),
+    PDFs = cms.PSet(
+        Zmumu_Model_Eta0 = Zmumu_Model,
+        Zmumu_Model_Eta1 = Zmumu_Model,
+        Zmumu_Model_Eta2 = Zmumu_Model,
+
+        Ztautau_Model_Eta0 = Ztautau_Model,
+        Ztautau_Model_Eta1 = Ztautau_Model,
+        Ztautau_Model_Eta2 = Ztautau_Model,
+    ),    
+    ## How to do the fit
+    binnedFit = cms.bool(True),
+    binsForFit = cms.uint32(15),
+    saveDistributionsPlot = cms.bool(False),
+    NumCPU = cms.uint32(1), ## leave to 1 for now, RooFit gives funny results otherwise
+    SaveWorkspace = cms.bool(True),
+)
+
+process.p1 = cms.Path(process.TnP_Muon_ID)
+
+
+
